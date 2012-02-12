064e52cc81ef51da1a82317433d9b07a3127a5f2
Magnar Sveen <magnars@gmail.com>
2012-02-12 19:10:32 +0100

    Initial commit

---
+++ test/extension-test.js
@@ -0,0 +1,36 @@
+var buster = require("buster");
+var ext = require("../lib/buster-lint").extension;
+var resourceSet = require("buster-resources").resourceSet;
+var analyzer = require("buster-analyzer");
+
+buster.testCase("Lint extension", {
+    setUp: function () {
+        this.resourceSet = resourceSet.create();
+        this.analyzer = analyzer.create();
+        this.listeners = { error: this.spy() };
+        this.analyzer.on("error", this.listeners.error);
+    },
+
+    "adds processor to resource set": function () {
+        var lint = ext.create();
+        this.spy(this.resourceSet, "addProcessor");
+        lint.beforeRun(this.resourceSet, this.analyzer);
+
+        assert.calledOnce(this.resourceSet.addProcessor);
+    },
+
+    "flags error on lint error": function (done) {
+        var lint = ext.create();
+        lint.beforeRun(this.resourceSet, this.analyzer);
+
+        this.resourceSet.addResource({
+            path: "/buster.js",
+            content: "var a = 123"
+        }).then(function (resource) {
+            resource.content().then(done(function (content) {
+                assert.calledOnceWith(this.listeners.error,
+                                      "Lint in /buster.js");
+            }.bind(this)));
+        }.bind(this));
+    }
+});
---
+++ test/lint-test.js
@@ -0,0 +1,39 @@
+var buster = require("buster");
+var lint = require("../lib/buster-lint").lint;
+
+buster.testCase('Lint', {
+    setUp: function () {
+        this.linter = lint.create({});
+    },
+
+    "should be ok without lint": function () {
+        var result = this.linter.check("var a = 123;");
+        assert(result.ok);
+    },
+
+    "should not be ok with lint": function () {
+        var result = this.linter.check("var a = 123");
+        refute(result.ok);
+    },
+
+    "should give anonymous filename if not given": function () {
+        var result = this.linter.check("var a = 123");
+        assert.equals(result.toString(),
+                      "[anonymous]:1:12 Expected ';' and instead saw '(end)'.");
+    },
+
+    "should give multiple errors": function () {
+        var result = this.linter.check("var a= 1;\nvar b= 2;", "myfile.js");
+        assert.equals(result.toString(),
+                      "myfile.js:1:6 Missing space between 'a' and '='.\n" +
+                      "myfile.js:2:6 Missing space between 'b' and '='.");
+    },
+
+    "should use given configuration": function () {
+        var linter = lint.create({
+            linterOptions: { eqeq: true }
+        });
+        var result = linter.check("var a = 1, b = 2, c = a == b;");
+        assert(result.ok);
+    }
+});
